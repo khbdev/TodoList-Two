c9abfdc5aa6cbecfe6d091d126adcbba4cd8c186
Azizbek <webcoderazizbek@gmail.com>
2026-02-18 19:54:42 -0200

    user-service

---
+++ user-service/pkg/hash.go
@@ -0,0 +1,8 @@
+package pkg
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+    hash, err := bcrypt.GenerateFromPassword([]byte(password), 10) 
+    return string(hash), err
+}
