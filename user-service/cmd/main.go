35da8be737add45d3b0c324bb37ed61950c0bb3e
Azizbek <webcoderazizbek@gmail.com>
2026-02-18 20:31:46 -0200

    user-service

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -2,45 +2,63 @@ package main
 
 import (
 	"log"
+	"net"
+	"os"
 	"time"
+
+	"google.golang.org/grpc"
+
 	"user-service/inrernal/cache"
 	"user-service/inrernal/config"
 	"user-service/inrernal/handler"
 	"user-service/inrernal/repository/postgres"
 	"user-service/inrernal/usecase"
-
 	"user-service/pkg"
+
+	userpb "github.com/khbdev/todolist-proto/proto/user"
 )
 
 func main() {
-	// env
+
 	pkg.LoadEnv()
 
-	// postgres
+
 	db, err := config.NewPostgresDB()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	
-	// redis
+
 	redisClient, err := config.NewRedisClient()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	
+	
+	repo := postgres.NewUserRepo(db)
+	userCache := cache.NewUserCache(redisClient)
+	srv := usecase.NewUserService(repo, userCache, 5*time.Minute)
+
+	grpcHandler := handler.NewUserGRPCHandler(srv)
 
 
-  repo :=  postgres.NewUserRepo(db)
-  cache := cache.NewUserCache(redisClient)
-   srv := usecase.NewUserService(repo, cache, 5*time.Minute)
-
-   hand := handler.NewUserGRPCHandler(srv)
-
-   
-
- 
+	grpcServer := grpc.NewServer()
 
 
+	userpb.RegisterUserServiceServer(grpcServer, grpcHandler)
+
+
+	addr := os.Getenv("PORT")
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("gRPC server running on %s", addr)
+
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
 }
