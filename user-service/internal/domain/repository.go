3ff32fc5c43e40278a1c73cd307885ce41769d3c
Azizbek <webcoderazizbek@gmail.com>
2026-02-27 22:24:18 -0200

    user-service

--- user-service/internal/domain/repository.go
+++ user-service/internal/domain/repository.go
@@ -1,9 +1,15 @@
 package domain
 
-import "user-service/internal/repositroy/model"
+import (
+	"context"
+	"user-service/internal/repositroy/model"
+)
 
 
 type UserRepository interface {
-	Create(user model.User) (error)
-	
+	Create( ctx context.Context, user model.User) (error)
+	GetAll(ctx context.Context) ([]model.User, error)
+	GetByID(ctx context.Context, user_id int) (model.User, error)
+	Update(ctx context.Context, user model.User) (model.User, error)
+	Delete(ctx context.Context, user_id int) (bool, error)
 }
