a3bf677d2c50801c879e86fcb1202fe82c8bc2c6
Azizbek <webcoderazizbek@gmail.com>
2026-03-26 12:31:29 -0200

    todolist

---
+++ email-service/internal/consumer/kafka.go
@@ -0,0 +1,61 @@
+package consumer
+
+import (
+	"context"
+	"emai-service/internal/domain"
+	"encoding/json"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type KafkaConsumer struct {
+	reader  *kafka.Reader
+	usecase domain.SendEmailUsecase
+}
+
+type KafkaTaskMessage struct {
+	UserId int64 `json:"user_id"`
+	TaskName string `json:"task_name"`
+}
+
+func NewKafkaConsumer(reader *kafka.Reader, usecase domain.SendEmailUsecase) *KafkaConsumer {
+	return &KafkaConsumer{
+		reader:  reader,
+		usecase: usecase,
+	}
+}
+
+func (c *KafkaConsumer) Consume(ctx context.Context) error {
+	log.Println("kafka consumer started...")
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("kafka consumer stopped")
+			return nil
+		default:
+			msg, err := c.reader.FetchMessage(ctx)
+			if err != nil {
+				log.Println("kafka fetch message error:", err)
+				continue
+			}
+
+			var data KafkaTaskMessage
+			if err := json.Unmarshal(msg.Value, &data); err != nil {
+				log.Println("kafka json unmarshal error:", err)
+				continue
+			}
+
+			if err := c.usecase.Execute(ctx, "kafka", data); err != nil {
+				log.Println("kafka usecase error:", err)
+				continue
+			}
+
+			if err := c.reader.CommitMessages(ctx, msg); err != nil {
+				log.Println("kafka commit error:", err)
+				continue
+			}
+		}
+	}
+}
--- email-service/internal/consumer/rabbitMq.go
+++ email-service/internal/consumer/rabbitMq.go
@@ -1,1 +1,78 @@
 package consumer
+
+import (
+	"context"
+	"emai-service/internal/config"
+	"emai-service/internal/domain"
+	"encoding/json"
+	"log"
+
+	"github.com/rabbitmq/amqp091-go"
+)
+
+type RabbitMQConsumer struct {
+	channel *amqp091.Channel
+	usecase domain.SendEmailUsecase
+}
+
+type RabbitMQAuthMessage struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func NewRabbitMQConsumer(rabbit *config.RabbitMQ, usecase domain.SendEmailUsecase) *RabbitMQConsumer {
+	return &RabbitMQConsumer{
+		channel: rabbit.Channel,
+		usecase: usecase,
+	}
+}
+
+func (c *RabbitMQConsumer) Consume(ctx context.Context) error {
+	queueName := "email_auth_queue"
+
+	msgs, err := c.channel.Consume(
+		queueName,
+		"",
+		false, 
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	log.Println("rabbitmq consumer started...")
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("rabbitmq consumer stopped")
+			return nil
+
+		case msg, ok := <-msgs:
+			if !ok {
+				log.Println("rabbitmq channel closed")
+				return nil
+			}
+
+			var data RabbitMQAuthMessage
+			if err := json.Unmarshal(msg.Body, &data); err != nil {
+				log.Println("json unmarshal error:", err)
+				_ = msg.Nack(false, false)
+				continue
+			}
+
+			if err := c.usecase.Execute(ctx, "rabbitmq", data); err != nil {
+				log.Println("usecase error:", err)
+				_ = msg.Nack(false, true)
+				continue
+			}
+
+			if err := msg.Ack(false); err != nil {
+				log.Println("ack error:", err)
+			}
+		}
+	}
+}
