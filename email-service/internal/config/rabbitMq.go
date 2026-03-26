322b2000b9fc0fddc6de51f5e134f0f4e362409a
Azizbek <webcoderazizbek@gmail.com>
2026-03-26 11:50:46 -0200

    email-service

---
+++ email-service/internal/config/rabbitMq.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"log"
+
+	"github.com/rabbitmq/amqp091-go"
+)
+
+type RabbitMQ struct {
+	Conn    *amqp091.Connection
+	Channel *amqp091.Channel
+}
+
+func NewRabbitMQ() *RabbitMQ {
+	conn, err := amqp091.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatal("rabbitmq connection error:", err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal("rabbitmq channel error:", err)
+	}
+
+	return &RabbitMQ{
+		Conn:    conn,
+		Channel: ch,
+	}
+}
